c32b3cd4a252eb49d0fb60ae71b36b00bd306cd6
Shivaraj Hiremath <shivaraj94826@gmail.com>
2025-12-10 11:35:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="font-mono text-4xl font-bold text-white">Welcome to Namma Prep</h1>
+    </div>
+  )
+}
+
+export default App
